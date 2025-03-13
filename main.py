a3e1046e9c049a0f140ca644a02d00540f105462
Ossi Noita <ossi.noita@tuni.fi>
2025-03-13 18:30:27 +0200

    Add dry running experiment dry

--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 
 import argparse
 import yaml
+import scipy.stats as sstats
+import numpy as np
 
 class Model:
     def __init__(self, full_args) -> None:
         print(full_args)
         self.exec = None
-        self.cwd = None
         self.param_key = ''
         self.pars = []
         for args in full_args:
@@ -19,23 +20,17 @@ class Model:
                 if self.param_key != '':
                     raise ValueError('Multiple model param_keys defined.')
                 self.param_key = args['param_key']
-            if 'cwd' in args:
-                if self.cwd != None:
-                    raise ValueError('Multiple model cwd defined')
-                self.cwd = args['cwd']
             if 'par' in args:
                 self.pars.append(args['par'])
         if self.param_key == '':
             self.param_key = '%#%'
         if self.exec == None:
             raise ValueError('exec not defined')
-        if self.cwd == None:
-            raise ValueError('cwd not defined')
 
     def __str__(self) -> str:
-        return f"{self.cwd} | {self.exec} {' '.join(self.pars)}" 
+        return f"{self.exec} {' '.join(self.pars)}" 
 
-    def dry(self, parameters) -> None:
+    def _create_command(self, parameters) -> str:
         command = f'{self.exec} {" ".join(self.pars)}'
         # Replace parameters
         idx = 1
@@ -46,7 +41,18 @@ class Model:
             else:
                 command = command.replace(to_replace, str(param))
             idx += 1
-        print(f'dry run of\n    {command}')
+        return command
+
+
+    def dry(self, cwd, parameters) -> None:
+        command = self._create_command(parameters)
+        print(f'dry run in {cwd} of\n    {command}')
+
+    def run(self, cwd, parameters) -> None:
+        command = self._create_command(parameters)
+        # TODO: create directory for running
+        # TODO: actually run the thing
+        print(f'dry run in {cwd} of\n    {command}')
 
 
 class Models:
@@ -68,8 +74,8 @@ class Models:
             ret_val.append(f'{id}: {model}')
         return '\n'.join(ret_val)
 
-    def dry(self, id, parameters) -> None:
-        self.models[id].dry(parameters)
+    def dry(self, id, cwd, parameters) -> None:
+        self.models[id].dry(cwd, parameters)
 
 
 class Experiment:
@@ -77,13 +83,35 @@ class Experiment:
         self.name = args['name']
         self.id = args['id']
         self.model = args['model']
+        self.cwd = args['cwd']
         self.parametrization = args['parametrization']
         self.cells = args['cells']
-        self.parametrization_length = args['parametrization_length']
+        self.parameter_count = args['parameter_count']
         self.manifest = args['manifest']
 
     def __str__(self) -> str:
         return self.id
+
+    def _get_directory(self, idx) -> str:
+        str_length = len(str(self.cells))
+        return self.name.replace('#', str(idx+1).rjust(str_length, '0'))
+
+    def _generate_parameters(self) -> np.ndarray:
+        if self.parametrization == 'latin_hybercube':
+            sampler = sstats.qmc.LatinHypercube(d=self.parameter_count, seed=0)
+            return sampler.random(n=self.cells)
+        else:
+            raise ValueError(f'parametrization "{self.parametrization}" not recognized')
+
+    def dry(self, models) -> None:
+        # generate all parameters
+        parameters = self._generate_parameters()
+
+        # run for each parameter
+        for idx in range(0, self.cells):
+            directory = self._get_directory(idx)
+            full_path = self.cwd + "/" + directory
+            models.dry(self.model, full_path, parameters[idx,:])
 
 
 class BiomarkerSet:
@@ -142,22 +170,27 @@ def run():
         print('Using config ', args.config)
     with open(args.config, 'r') as f:
         content = yaml.safe_load(f)
-        models = Models(content['model'])
-        experiment = Experiment(content['experiment'][0])
-        biomarkers = Biomarkers(content['biomarkers'])
-        calibration = Calibration(content['calibration'])
-        print('\n--models--')
-        print(models)
-        print('\n--experiment--')
-        print(experiment)
-        print('\n--biomarkers--')
-        print(biomarkers)
-        print('\n--calibration--')
-        print(calibration)
+    models = Models(content['model'])
+    experiment = Experiment(content['experiment'][0])
+    biomarkers = Biomarkers(content['biomarkers'])
+    calibration = Calibration(content['calibration'])
+    print('\n--models--')
+    print(models)
+    print('\n--experiment--')
+    print(experiment)
+    print('\n--biomarkers--')
+    print(biomarkers)
+    print('\n--calibration--')
+    print(calibration)
 
-        print('\n--script-running--')
-        models.dry('my_matlab_model', [42,43])
+    print('\n--script-running--')
 
+    # Run all experiments
+    experiment.dry(models)
+
+    # Calculate biomarkers for each `target` instance
+
+    # Go through each 
 
 
 if __name__ == '__main__':
