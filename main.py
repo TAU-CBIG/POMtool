c5594630718efe1e3e436b48768ac80b15a0cc5f
Ossi Noita <ossi.noita@tuni.fi>
2025-04-27 18:00:26 +0300

    Add silent mode

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def run():
             )
     parser.add_argument('--config', required=True, help='Config file to do the thing')
     parser.add_argument('--dry', action='store_true',help='Run without actually doing the experiment.')
+    parser.add_argument('--silent', action='store_true',help='Silent control flow printing')
     parser.add_argument('--skip-experiment', action='store_true',help='Skip experiment, it is assumed you already have run experiment')
     parser.add_argument('--only-experiment', action='store_true',help='Only run experiment')
     parser.add_argument('--skip-biomarkers', action='store_true',help='Skip biomarkers, following steps might expect biomarkers to exist')
@@ -23,8 +24,11 @@ def run():
     parser.add_argument('--skip-calibration', action='store_true',help='Skip calibration')
     parser.add_argument('--only-calibration', action='store_true',help='Only run calibration, assumes you have run previous steps already and have the data')
     args = parser.parse_args()
+    def noprint(*values: object,):
+        pass
+    myprint = noprint if args.silent else print
     if args.config:
-        print(f'Using config `{args.config}`')
+        myprint(f'Using config `{args.config}`')
     with open(args.config, 'r') as f:
         content = yaml.safe_load(f)
     if args.only_experiment:
@@ -39,14 +43,14 @@ def run():
     models = mod.Models(content['model'])
     experiment = exp.Experiment(content['experiment'][0])
     if not args.skip_experiment:
-        print('Start experiments')
+        myprint('Start experiments')
         if not args.dry:
             experiment.run(models)
         else:
             experiment.dry(models)
-        print('End experiments')
+        myprint('End experiments')
     if not args.skip_biomarkers:
-        print('Start biomarkers')
+        myprint('Start biomarkers')
         biomarkers = bm.Biomarkers(content['biomarkers'])
         if args.skip_experiment:
             experiment.empty_run(models)
@@ -54,15 +58,15 @@ def run():
             biomarkers.run(experiment)
         else:
             biomarkers.dry(experiment)
-        print('End biomarkers')
+        myprint('End biomarkers')
     if not args.skip_calibration:
-        print('Start calibration')
+        myprint('Start calibration')
         if not args.dry:
             calibration = cal.Calibration(content['calibration'])
             calibration.run()
         else:
             print(content['calibration'])
-        print('End calibration')
+        myprint('End calibration')
 
 
 if __name__ == '__main__':
