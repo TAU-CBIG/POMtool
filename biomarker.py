ef07d0f1fa8d26607084deb7b51fd9a5f9e50823
Ossi Noita <ossi.noita@tuni.fi>
2025-05-16 15:01:06 +0300

    Add dv_dt_max biomarker as config option

--- biomarker.py
+++ biomarker.py
@@ -538,6 +538,7 @@ BIOMARKERS = {'MDP': MDP(),
               'Min_Cai': Min_Cai(),
               'Rate_Cai': Rate_Cai(),
               'CL': CL(),
+              'dv_dt_max': dv_dt_max(),
               } | { str(APD_N(val)): APD_N(val) for val in APD_VALUES_OF_N}
 
 
