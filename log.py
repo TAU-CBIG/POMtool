4ff7b61c2d7b7136eb8d15e34e3814cbfc3af1b1
Ossi Noita <ossi.noita@tuni.fi>
2025-07-08 17:07:06 +0300

    Add logger functions to print only once

--- log.py
+++ log.py
@@ -8,3 +8,17 @@ def print_verbose(*args):
 def print_info(*args):
     if INFO:
         print(*args)
+
+already_printed = set()
+def print_once(*args):
+    if not (args in already_printed):
+        already_printed.add(args)
+        print(*args)
+
+def print_once_verbose(*args):
+    if VERBOSE:
+        print_once(*args)
+
+def print_once_info(*args):
+    if INFO:
+        print_once(*args)
