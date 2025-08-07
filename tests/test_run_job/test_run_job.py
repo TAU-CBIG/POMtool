c88d98a320dc068d1bb2787df913f3e05b63ba8d
oYlinen <olli.ylinen@outlook.com>
2025-08-07 09:32:29 +0300

    Add test_discarded

--- tests/test_run_job/test_run_job.py
+++ tests/test_run_job/test_run_job.py
@@ -9,3 +9,13 @@ def test_biomarkers():
     gold = run_job_utility.get_gold(file="run_job/biomarkers.csv")
 
     assert gold == lead
+
+
+def test_discarded():
+    lead = run_job_utility.get_lead(file="run_job/discarded.csv",
+                                    config="test_config_run_job.yaml",
+                                    patch_count=1,
+                                    patch_idx=0)
+    gold = run_job_utility.get_gold(file="run_job/discarded.csv")
+
+    assert gold == lead
