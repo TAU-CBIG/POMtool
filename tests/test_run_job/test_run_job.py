60c3cf3efce11d20ddbf869b75dad0fab2c01dd0
oYlinen <olli.ylinen@outlook.com>
2025-08-07 09:34:12 +0300

    Add test_discarded_because_of_nans

--- tests/test_run_job/test_run_job.py
+++ tests/test_run_job/test_run_job.py
@@ -19,3 +19,13 @@ def test_discarded():
     gold = run_job_utility.get_gold(file="run_job/discarded.csv")
 
     assert gold == lead
+
+
+def test_discarded_because_of_nans():
+    lead = run_job_utility.get_lead(file="run_job/discarded_because_of_nans.csv",
+                                    config="test_config_run_job.yaml",
+                                    patch_count=1,
+                                    patch_idx=0)
+    gold = run_job_utility.get_gold(file="run_job/discarded_because_of_nans.csv")
+
+    assert gold == lead
