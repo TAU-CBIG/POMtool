b6b8bcb93ac311823f9e6dff8a9f2bdd29ee0951
oYlinen <olli.ylinen@outlook.com>
2025-08-07 10:12:06 +0300

    Add --make-gold option to tests with pytest

--- tests/test_run_job/conftest.py
+++ tests/test_run_job/conftest.py
@@ -4,9 +4,15 @@ import run_job_utility
 
 def pytest_addoption(parser):
     parser.addoption("--skip-lead", action="store_true", help="A custom option for pytest")
+    parser.addoption("--make-gold", action="store_true", help="A custom option for pytest")
 
 
 @pytest.fixture(scope='session')
 def job_session_fixture(request):
     run_job_utility.skip = (request.config.getoption("--skip-lead"))
     yield
+
+    make_gold = (request.config.getoption("--make-gold"))
+    if make_gold:
+        for f in set(run_job_utility.gold_files):
+            run_job_utility.make_gold_file(f)
--- tests/test_run_job/run_job_utility.py
+++ tests/test_run_job/run_job_utility.py
@@ -7,6 +7,7 @@ import yaml
 import os
 import shutil
 import re
+import datetime
 
 skip = False
 GOLD = "gold"
@@ -31,7 +32,7 @@ def get_lead(config, file, patch_count=1, patch_idx=0) -> str:
                 "--silent"]
 
     if not skip and argument not in ALREADY_RAN:
-        #this is because of a bug: cwd should always be where --config=foo.yaml is -> fix this at some point
+        # this is because of a bug: cwd should always be where --config=foo.yaml is -> fix this at some point
         shutil.copytree(original_path, dir_lead, dirs_exist_ok=True)
         try:
             os.chdir(dir_lead)
@@ -44,7 +45,11 @@ def get_lead(config, file, patch_count=1, patch_idx=0) -> str:
     return data
 
 
+gold_files = []
+
+
 def get_gold(file) -> str:
+    gold_files.append(file)
     gold_dir = pathlib.Path("../data/gold/")
     file_gold = gold_dir / file
 
@@ -101,3 +106,36 @@ def get_params_from_text(text) -> list:
     # Matches to "number.number"
     params = re.findall("[0-9]{1,}\\.[0-9]{1,}", text)
     return params
+
+
+def log_gold(file: pathlib.Path, function: str):
+    log_file = "../data/gold/logs.txt"
+    with open(log_file, 'a') as f:
+        f.write(f"{function} {file} : {datetime.datetime.now()}\n")
+
+
+def diff_of_files(file1, file2) -> bool:
+    try:
+        with open(file1, 'r') as f1:
+            with open(file2, 'r') as f2:
+                content1 = f1.read()
+                content2 = f2.read()
+                return content1 == content2
+    except:
+        return False
+
+
+def make_gold_file(file: str):
+    file_lead = pathlib.Path("../data/lead/" + file)
+    file_gold = pathlib.Path("../data/gold/" + file)
+    file_gold.parent.mkdir(exist_ok=True, parents=True)
+
+    if file_gold.exists():
+        if diff_of_files(file_gold, file_lead):
+            return  # Nothing to do
+
+        log_gold(file_gold, function="Change")
+        os.remove(file_gold)
+    else:
+        log_gold(file_gold, function="Add")
+    shutil.copy(file_lead, file_gold)
